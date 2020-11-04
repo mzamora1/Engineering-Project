7189b59ce5111fc8d97a4db66cd8b4d8f80d82ac
mzamora1 <58890029+mzamora1@users.noreply.github.com>
2020-11-04 13:10:42 -0700

    Update Exercise3.js

--- Exercise3.js
+++ Exercise3.js
@@ -1,49 +1,73 @@
+function setup() {
+  createCanvas(400, 400);
+}
+
 let positionArray = [];
-function setup(){
-    createCanvas(400,400);
-    for(let i = 0; i < 10; i++){
-        positionArray.push({x: 20+i*40, y: 100+i*20}); //fill positionArray will objects holding the x and y coordinates of each dot
-        circle(positionArray[i].x, positionArray[i].y, 5);
-    }
+  positionArray.push({x: 205, y:56,});
+  positionArray.push({x: 178, y:94}); 
+  positionArray.push({x: 225, y:95}); 
+  positionArray.push({x: 202, y:126}); 
+  positionArray.push({x: 166, y:127}); 
+  positionArray.push({x: 234, y:126}); 
+  positionArray.push({x: 159, y:158}); 
+  positionArray.push({x: 246, y:158}); 
+
+let fishArray = [];
+  fishArray.push({x: 129, y:109}); 
+  fishArray.push({x: 129, y:133}); 
+  fishArray.push({x: 129, y:152}); 
+  fishArray.push({x: 150, y:145}); 
+  fishArray.push({x: 147, y:114}); 
+  fishArray.push({x: 163, y:129}); 
+  fishArray.push({x: 183, y:110}); 
+  fishArray.push({x: 218, y:89}); 
+  fishArray.push({x: 266, y:86}); 
+  fishArray.push({x: 295, y:104}); 
+  fishArray.push({x: 190, y:137}); 
+  fishArray.push({x: 226, y:152}); 
+  fishArray.push({x: 269, y:150}); 
+  fishArray.push({x: 291, y:134}); 
+
+let visitedboundaries = [];
+
+function draw() {
+
+  for(let i = 0; i < positionArray.length; i++){
+    circle(positionArray[i].x, positionArray[i].y, 5);
+  }
+  
+    stroke(20);
+  if (mouseIsPressed === true) {
+    line(mouseX, mouseY, pmouseX, pmouseY);
+    checkAllBoundaries(21);
+  }
+
 }
 
-function draw(){
-    clear();
-    text("screen will clear if mouse is pressed/dragged while not in a boundary", 20,20);
-    for(let i = 0; i < positionArray.length; i++){
-        circle(positionArray[i].x, positionArray[i].y, 3);
-    }
-    if(mouseIsPressed){
-        checkAllBoundaries(17);
-    }
-}
-
-function mouseReleased(){
-    circle(mouseX, mouseY, 3);
-        console.log(mouseX,  mouseY);
-}
-
-//clears the screen if mouse is not within any boundaries 
 function checkAllBoundaries(size){
     let inAnyBoundary = false;
-    for(let i = 0; i < positionArray.length; i++){ //checks all boundaries
+      for(let i = 0; i < positionArray.length; i++){
         let boundary = {
             left: positionArray[i].x - size,
             right: positionArray[i].x + size,
             top: positionArray[i].y - size,
             bottom: positionArray[i].y + size
         }
-        rect(boundary.left, boundary.top, size*2); //for testing purposes
-        if(mouseX > boundary.left && mouseX < boundary.right && mouseY < boundary.bottom && mouseY > boundary.top){
-           // console.log("you in da boundary foo");
-            inAnyBoundary = true;
+        //rect(boundary.left, boundary.top, size*2); //for testing purposes
+        if(mouseX > boundary.left && mouseX < boundary.right && mouseY < boundary.bottom && mouseY >           boundary.top){
+          inAnyBoundary = true;
+          if (!visitedboundaries.includes(positionArray[i])){
+            visitedboundaries.push(positionArray[i]);
+            console.log("hi");
+          }
+         }
+      }
+        if (visitedboundaries.length >= positionArray.length){
+          positionArray = fishArray;
+          console.log("lo hizimos!");
         }
-    }
-    if(!inAnyBoundary){ //if not in bounds of any dot then clear the screen
-        //console.log("not in bounds");
+        if(!inAnyBoundary){ 
+        console.log("not in bounds");
         clear();
     }
 }
-
-
-
